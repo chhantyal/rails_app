3cf46d5a5b97641d3d72f1e568f631572dbb11a1
Nar Kumar <nkchhantyal@gmail.com>
2012-03-08 17:57:14 +0530

    started testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
