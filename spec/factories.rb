63d074e4696c4034f928633c85b15e1b8fda931e
Nar Kumar <nkchhantyal@gmail.com>
2012-03-20 02:13:15 +0530

    Finished user sign-up pagae

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+#Didn't load automatically, so I added manually.'
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
